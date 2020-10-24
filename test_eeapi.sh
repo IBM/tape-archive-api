927f64de0915fa6d76608d41854c5aef46e631c8
N. Haustein <haustein@de.ibm.com>
2020-10-24 11:16:57 +0200

    adjusted endpoints

--- test_eeapi.sh
+++ test_eeapi.sh
@@ -14,24 +14,37 @@ else
 fi
 
 # this includes all routes to test
-commands="about test status info/tape info/node info/pool info/library tasks/all tasks/active taskshow/1054 filestate/gpfs/fs1/ee/dir0/file0 recall migrate"
+commands="about test status info/tape info/node info/pool info/library tasks/all tasks/active taskshow/1054 filestate recall migrate"
+
+fileList=$(echo -e "/gpfs/fs1/ee/test1/file_0.pdf\n/gpfs/fs1/ee/test1/file_1.pdf\n/gpfs/fs1/ee/test1/file_2.pdf\n/gpfs/fs1/ee/test1/file _ 6 .pdf\n/gpfs/fs1/ee/test1/file_7.pdf")
 
 urlprefix="http://localhost:"$port"/"
 
 for cmd in $commands;
 do
   url="$urlprefix$cmd"
-  if [[ "$cmd" != "recall" && "$cmd" != "migrate" ]];
+  if [[ "$cmd" != "recall" && "$cmd" != "migrate" && "$cmd" != "filestate" && "$cmd" != "runpolicy" ]];
   then 
     curlcmd="curl -X GET "$url
   else
-    curlcmd="curl -X PUT "$url
+    if [[ "$cmd" == "filestate" ]]; 
+	then
+	  op=GET
+	else
+	  op=PUT
+	fi
+    if [[ "$cmd" == "migrate" ]]; 
+	then
+	  url=$url"?pool=pool1"
+	fi
+
+    curlcmd="curl -X $op "$url" -d \"$fileList\""
   fi 
   echo "Command: $curlcmd"
   eval "$curlcmd"
   echo "===================================================================="
   echo "Press Enter (CTRL-C to finish)"
-  read a 
+#  read a 
 done
 
 exit 0
